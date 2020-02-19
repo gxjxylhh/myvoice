682f018ed8e49179f762751c054de9176802e75b
Xingjian Guo <353583240@qq.com>
2020-02-19 15:02:09 +1100

    simple chatting box with no backend yet

--- App.js
+++ App.js
@@ -1,43 +1,47 @@
 //Example to play music in React Native
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 //Import React
 import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ScrollView,
-  SafeAreaView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    ScrollView,
+    SafeAreaView,
 } from 'react-native';
 //Import basic elements we need from React Native
 import Sound from 'react-native-sound';
 //Import library for Sound Component
 
-//List of the dummy sound track
+import {GiftedChat} from 'react-native-gifted-chat'
+
+
 const audioList = [
-  {
-    title: 'Play mp3 sound from Local',
-    isRequire: true,
-    url: require('./music.mp3'),
-  },
+    {
+        title: 'Play mp3 sound from Local',
+        isRequire: true,
+        url: require('./music.mp3'),
+    },
 ];
 
 var sound1, sound2, sound3, sound4, sound5, sound6;
 
 function playSound(item, index) {
-  if (index == 0) {
-    sound1 = new Sound(item.url, (error, sound) => {
-      if (error) {
-        alert('error' + error.message);
-        return;
-      }
-      //make sure that only 1 sound will be playing at a time
-      //below should be copied to other sound files when they will be played
-      if (global.sound) global.sound.stop();
-      global.sound = sound1;
-      sound1.play( success => { console.log('Playing') } );
-    });
-  }
+    if (index == 0) {
+        sound1 = new Sound(item.url, (error, sound) => {
+            if (error) {
+                alert('error' + error.message);
+                return;
+            }
+            //make sure that only 1 sound will be playing at a time
+            //below should be copied to other sound files when they will be played
+            if (global.sound) global.sound.stop();
+            global.sound = sound1;
+            sound1.play(success => {
+                console.log('Playing')
+            });
+        });
+    }
 // else if (index == 1) {
 //    sound2 = new Sound(item.url, '', (error, sound) => {
 //      if (error) {
@@ -52,11 +56,11 @@ function playSound(item, index) {
 }
 
 function stopSound(item, index) {
-  if (index == 0 && sound1) {
-    sound1.stop(() => {
-      console.log('Stop');
-    });
-  }
+    if (index == 0 && sound1) {
+        sound1.stop(() => {
+            console.log('Stop');
+        });
+    }
 // else if (index == 1 && sound2) {
 //    sound2.stop(() => {
 //      console.log('Stop');
@@ -64,103 +68,142 @@ function stopSound(item, index) {
 //  }
 }
 
-function componentWillMount(){
-  //sound1.stop();
-
-}
-
-
-function componentWillUnmount() {
-  sound1.release();
-//  sound2.release();
-
-}
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    Sound.setCategory('Playback', true); // true = mixWithOthers
-    this.state = {
-      tests: {},
-    };
-  }
 
-  render() {
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <View style={styles.container}>
-          <Text style={styles.headerTitle}>
-            Gift for you
-          </Text>
-          <ScrollView style={styles.container}>
-            {audioList.map((item, index) => {
-              return (
-                <View style={styles.feature} key={item.title}>
-                  <Text style={{ flex: 1, fontSize: 14 }}>{item.title}</Text>
-                  <TouchableOpacity
-                    onPress={() => {
-                      return playSound(item, index);
-                    }}>
-                    <Text style={styles.buttonPlay}>Play</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={() => {
-                      return stopSound(item, index);
-                    }}>
-                    <Text style={styles.buttonStop}>Stop</Text>
-                  </TouchableOpacity>
+
+
+
+    constructor(props) {
+        super(props);
+        Sound.setCategory('Playback', true); // true = mixWithOthers
+        this.state = {
+            tests: {},
+            messages:[],
+        };
+    }
+
+
+     componentWillMount() {
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: "Hello developer",
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: "React Native",
+                        avatar: "https://placeimg.com/140/140/any",
+                    },
+                },
+            ],
+        });
+    }
+
+     componentWillUnmount() {
+     //   sound1.release();
+    //  sound2.release();
+
+    }
+
+     onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
+
+
+    render() {
+        return (
+            <SafeAreaView style={{flex: 1}}>
+                <View style={styles.container}>
+                    <Text style={styles.headerTitle}>
+                        Gift for you
+                    </Text>
+                    <ScrollView style={styles.container}>
+                        {audioList.map((item, index) => {
+                            return (
+                                <View style={styles.feature} key={item.title}>
+                                    <Text style={{flex: 1, fontSize: 14}}>{item.title}</Text>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            return playSound(item, index);
+                                        }}>
+                                        <Text style={styles.buttonPlay}>Play</Text>
+                                    </TouchableOpacity>
+                                    <TouchableOpacity
+                                        onPress={() => {
+                                            return stopSound(item, index);
+                                        }}>
+                                        <Text style={styles.buttonStop}>Stop</Text>
+                                    </TouchableOpacity>
+                                </View>
+                            );
+                        })}
+                    </ScrollView>
+
+                    <GiftedChat
+                        messages={this.state.messages}
+                        onSend={messages => this.onSend(messages)}
+                        user={{
+                            _id: 1,
+                        }}
+                    />
+
                 </View>
-              );
-            })}
-          </ScrollView>
-        </View>
-      </SafeAreaView>
-    );
-  }
+
+
+            </SafeAreaView>
+
+
+        );
+    }
 }
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  headerTitle: {
-    fontSize: 16,
-    color: 'white',
-    fontWeight: 'bold',
-    paddingVertical: 20,
-    textAlign: 'center',
-    backgroundColor: 'rgba(00,00,80,1)',
-  },
-  buttonPlay: {
-    fontSize: 16,
-    color: 'white',
-    backgroundColor: 'rgba(00,80,00,1)',
-    borderWidth: 1,
-    borderColor: 'rgba(80,80,80,0.5)',
-    overflow: 'hidden',
-    paddingHorizontal: 15,
-    paddingVertical: 7,
-  },
-  buttonStop: {
-    fontSize: 16,
-    color: 'white',
-    backgroundColor: 'rgba(80,00,00,1)',
-    borderWidth: 1,
-    borderColor: 'rgba(80,80,80,0.5)',
-    overflow: 'hidden',
-    paddingHorizontal: 15,
-    paddingVertical: 7,
-  },
-  feature: {
-    flexDirection: 'row',
-    padding: 10,
-    alignSelf: 'stretch',
-    alignItems: 'center',
-    borderTopWidth: 1,
-    borderTopColor: 'rgb(180,180,180)',
-    borderBottomWidth: 1,
-    borderBottomColor: 'rgb(230,230,230)',
-  },
+    container: {
+        flex: 1,
+    },
+    headerTitle: {
+        fontSize: 16,
+        color: 'white',
+        fontWeight: 'bold',
+        paddingVertical: 20,
+        textAlign: 'center',
+        backgroundColor: 'rgba(00,00,80,1)',
+    },
+    buttonPlay: {
+        fontSize: 16,
+        color: 'white',
+        backgroundColor: 'rgba(00,80,00,1)',
+        borderWidth: 1,
+        borderColor: 'rgba(80,80,80,0.5)',
+        overflow: 'hidden',
+        paddingHorizontal: 15,
+        paddingVertical: 7,
+    },
+    buttonStop: {
+        fontSize: 16,
+        color: 'white',
+        backgroundColor: 'rgba(80,00,00,1)',
+        borderWidth: 1,
+        borderColor: 'rgba(80,80,80,0.5)',
+        overflow: 'hidden',
+        paddingHorizontal: 15,
+        paddingVertical: 7,
+    },
+    feature: {
+        flexDirection: 'row',
+        padding: 10,
+        alignSelf: 'stretch',
+        alignItems: 'center',
+        borderTopWidth: 1,
+        borderTopColor: 'rgb(180,180,180)',
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgb(230,230,230)',
+    },
 });
--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# To run
+# To run:
 
 1.Start a virtual android machine first.
 
 2.In terminal input as follow: sudo npx react-native run-android.
 
 3.Cross fingers and hope it works.
+
+#Workload:
+
+1.Implemented initial chatting UI according to gifted-chat
+
+2.integrate with playing sound/music locally as a feature
+
+#To Do:
+
+1.Implement chatting backend maybe?(unless the structure is meant to be a single player mode :D)
+
+2.Recording audios :P yes for sure!
+
+3.Think of new features
